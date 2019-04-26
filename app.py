34a33aff23bd109279501e9d35b3cc169976efe6
lamoboos223 <lamo.boos@hotmail.com>
2019-04-26 19:18:28 +0300

    add more features

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+from IPython.display import Markdown, display
 
 
 class NeuralNetwork():
@@ -8,7 +9,11 @@ class NeuralNetwork():
         np.random.seed(1)
 
         # converting weights to a 3 by 1 matrix with values from -1 to 1 and mean of 0
-        self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
+        # this is how it'll look like
+        # [number,
+        #  number,
+        #  number]
+        self.synaptic_weights = 2 * np.random.random((5, 1)) - 1
 
     def sigmoid(self, x):
         # applying the sigmoid function to calculate the activation function
@@ -23,9 +28,13 @@ class NeuralNetwork():
         # training the model to make accurate predictions while adjusting weights continually
         for iteration in range(training_iterations):
             # siphon the training data via  the neuron
+
+            # 1. Forward propagation
             output = self.predict(training_inputs)
 
-            self.BackPropagation(output, training_inputs, training_outputs)
+            # 2. Back propagation
+            self.synaptic_weights += self.BackPropagation(
+                output, training_inputs, training_outputs)
 
     def BackPropagation(self, output, training_inputs, training_outputs):
 
@@ -36,7 +45,7 @@ class NeuralNetwork():
         adjustments = np.dot(training_inputs.T, error *
                              self.sigmoid_derivative(output))
 
-        self.synaptic_weights += adjustments
+        return adjustments
 
     def predict(self, inputs):
         # passing the inputs via the neuron to get output
@@ -45,6 +54,9 @@ class NeuralNetwork():
         inputs = inputs.astype(float)
         output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
         return output
+
+    def printmd(self, msg):
+        display(Markdown(msg))
 
 
 if __name__ == "__main__":
@@ -56,12 +68,18 @@ if __name__ == "__main__":
     print(neural_network.synaptic_weights)
 
     # training data consisting of 4 examples--3 features and 1 output
-    training_inputs = np.array([[0, 0, 1],
-                                [1, 1, 1],
-                                [1, 0, 1],
-                                [0, 1, 1]])
+    training_inputs = np.array([[0, 1, 1, 1, 0],
+                                [1, 1, 0, 0, 0],
+                                [1, 0, 0, 0, 1],
+                                [0, 0, 0, 0, 1],
+                                [1, 1, 0, 0, 1],
+                                [0, 0, 1, 1, 0],
+                                [0, 1, 0, 1, 0],
+                                [1, 1, 0, 1, 1]])
 
-    training_outputs = np.array([[0, 1, 1, 0]]).T
+    # the class vector has to be column vector so we used T to transpose it
+    # 0 --> Cat  | 1 --> Dog
+    training_outputs = np.array([[0, 0, 1, 1, 1, 0, 0, 1]]).T
 
     # training taking place
     neural_network.train(training_inputs, training_outputs, 15000)
@@ -69,15 +87,20 @@ if __name__ == "__main__":
     print("Ending Weights After Training: ")
     print(neural_network.synaptic_weights)
 
-    user_input_one = str(input("User Input One: "))
-    user_input_two = str(input("User Input Two: "))
-    user_input_three = str(input("User Input Three: "))
+    neural_network.printmd("**0 --> No\n 1 --> yes**")
+    user_input_one = str(input("Is it big? "))
+    user_input_two = str(input("Is it sensetive?  "))
+    user_input_three = str(input("Does it like weird places? "))
+    user_input_four = str(input("Does it give birth alot? "))
+    user_input_five = str(input("Does it guard your house?"))
 
     print("Your features are: ", user_input_one,
-          user_input_two, user_input_three)
+          user_input_two, user_input_three, user_input_four, user_input_five)
 
     result = neural_network.predict(
-        np.array([user_input_one, user_input_two, user_input_three]))
+        np.array([user_input_one, user_input_two, user_input_three,
+                  user_input_four, user_input_five]))
+
     result = round(result[0])
 
     print("The output is: ")
